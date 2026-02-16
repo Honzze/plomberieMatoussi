17ebe7c875f86d4232829f5ea3820e57bdebb262
Honzze <95212442+Honzze@users.noreply.github.com>
2026-02-16 12:36:49 +0100

    Add files via upload

---
+++ src/components/landing/CardEngagement.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Image from "next/image";
+
+type CardEngagementProps = {
+    title: string;
+    description: string;
+    imageUrl: string;
+}
+
+const CardEngagement = ({ title, description, imageUrl }:CardEngagementProps) => {
+    return (
+        <div className="flex flex-col justify-center items-center max-w-sm rounded-lg overflow-hidden ">
+            <h2 className="font-bold text-xl text-center mb-2">{title}</h2>
+            <Image className="w-20 h-auto "
+                   src={imageUrl}
+                   alt="Image de la carte"
+            />
+            <div className="px-6 py-4">
+                <p className="text-gray-700 text-base">{description}</p>
+            </div>
+            <div className="px-6 py-4 flex justify-end">
+
+            </div>
+        </div>
+
+    );
+};
+
+export default CardEngagement;
